d62b8931469736e81baf4b54d2c3f71844694b3c
Huzifa12 <136724205+Huzifa12@users.noreply.github.com>
2023-06-15 08:10:27 -0700

    Create channelMode.js

---
+++ server/models/channelMode.js
@@ -0,0 +1,30 @@
+const { sequelize, Sequelize } = require('../server')
+const User = require('./userModel'); 
+
+const Channel = sequelize.define('Channel', {
+  ChannelId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  UserID:{
+    type:Sequelize.INTEGER,
+    allowNull:false,
+  },
+  B_email: {
+    type: Sequelize.STRING(255),
+    allowNull: true,
+  },
+  descriptions: {
+    type: Sequelize.STRING(255),
+    allowNull: true,
+  },
+  Banner_Pic: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+});
+Channel.belongsto(User, { foreignKey: 'UserID' });
+module.exports = Channel;
+
