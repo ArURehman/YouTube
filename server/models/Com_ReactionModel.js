65d699fad84cabeffb6cb025052ab0235d91341d
Huzifa12 <136724205+Huzifa12@users.noreply.github.com>
2023-06-15 08:17:24 -0700

    Create Com_ReactionModel.js

---
+++ server/models/Com_ReactionModel.js
@@ -0,0 +1,31 @@
+const { sequelize, Sequelize } = require('../server');
+const Comment=require('./CommentModel');
+const User = require('./userModel'); 
+
+const Com_Reaction = sequelize.define('Com_Reaction', {
+    ComID:{
+        type:Sequelize.integer,
+        allowNull:false,
+        primarykey:true,
+    },
+    
+    UserID:{
+      type:Sequelize.integer,
+      allowNull:false,
+      primarykey:true,
+    },
+    Reaction:{
+        type:Sequelize.BOOLEAN,
+        allowNull:true,
+    },
+    Reaction_At:{
+        type:Sequelize.DATE,
+        allowNull:true,
+    },
+
+  });
+  
+  Com_Reaction.belongsto(User,{foreignkey:'UserID'});
+  Com_Reaction.belongsto(Comment,{foreignkey:'ComID'});
+
+  module.exports=Com_Reaction;
