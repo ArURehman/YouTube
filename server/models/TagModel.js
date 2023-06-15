f9960ece4620b617add962b0ef5bdc544f47cd3c
Huzifa12 <136724205+Huzifa12@users.noreply.github.com>
2023-06-15 08:14:30 -0700

    Create TagModel.js

---
+++ server/models/TagModel.js
@@ -0,0 +1,19 @@
+const { sequelize, Sequelize } = require('../server');
+const Video = require('./videoModel'); 
+
+const Tag= sequelize.define('Tag', {
+
+    VideoID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+    
+    TagText:{
+        type:Sequelize.STRING(255),
+        allowNull:true,
+    }
+  });
+
+  Tag.belongsto(Video, { foreignKey: 'VideoID' });
+  module.expors=Tag;
