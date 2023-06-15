039e7e95cd7596b96d681720d66545f4adb4fd0d
Huzifa12 <136724205+Huzifa12@users.noreply.github.com>
2023-06-15 08:13:58 -0700

    Create viewedModel.js

---
+++ server/models/viewedModel.js
@@ -0,0 +1,28 @@
+const { sequelize, Sequelize } = require('../server');
+
+const User = require('./userModel'); 
+const Video = require('./videoModel'); 
+	
+const Viewed = sequelize.define('Viewed', {
+    
+    UserID:{
+      type:Sequelize.integer,
+      allowNull:false,
+      primaryKey:true,
+    },
+    VideoID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+    
+    Viewed_At:{
+        type:Sequelize.DATE,
+        allowNull:false,
+    }
+  });
+
+  Viewed.belongsto(User, { foreignKey: 'UserID' });
+  Viewed.belongsto(Video, { foreignKey: 'VideoID' });
+
+  module.exports=Viewed;
