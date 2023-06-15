5a41f7e467218c9cfd222a7b6f7e1d108a86476f
Huzifa12 <136724205+Huzifa12@users.noreply.github.com>
2023-06-15 08:15:06 -0700

    Create subscriberModel.js

---
+++ server/models/subscriberModel.js
@@ -0,0 +1,34 @@
+const { sequelize, Sequelize } = require('../server')
+const User = require('./userModel'); 
+const Channel = require('./channelModel'); 
+	
+const Subscriber=sequelize.define('Subscriber', {
+    UserId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      
+    },
+    ChannelID:{
+      type:Sequelize.integer,
+      allowNull:false,
+      primarykey:true,
+    },
+    Subbed_At:{
+        type:Sequelize.DATE,
+        allowNull:false,
+    }
+  });
+  
+  User.belongsToMany(Channel, {
+    through: Subscriber,
+    foreignKey: 'UserId',
+    otherKey: 'ChannelID',
+  });
+  Channel.belongsToMany(User, {
+    through: Subscriber,
+    foreignKey: 'ChannelID',
+    otherKey: 'UserId',
+  });
+   module.exports=Subscriber;
+
