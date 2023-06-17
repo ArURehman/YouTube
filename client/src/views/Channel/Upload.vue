f2f3ed0c7682ae1fad1301b727d6d1170d63bc00
ArURehman <ajeebareeb0147@gmail.com>
2023-06-18 03:06:01 +0500

    Fixed bugs. Can upload videos now

--- client/src/views/Channel/Upload.vue
+++ client/src/views/Channel/Upload.vue
@@ -1,47 +1,50 @@
 <template>
     <div class="text-white px-40">
         <div class="mb-5">
-            <h4 class="mt-2 mb-4 text-xl font-medium ml-1">Details:</h4>
-            <textarea maxlength="50" v-model="title" placeholder="Video Title" class="h-16 text-sm w-full min-h-10 p-2 bg-searchBarGray rounded overflow-hidden break-words resize-x whitespace-normal outline-none text-textWhite"></textarea>
-            <textarea maxlength="250" v-model="description" placeholder="Video Description" class="h-24 text-sm w-full min-h-10 p-2 bg-searchBarGray rounded overflow-hidden break-words resize-x whitespace-normal outline-none text-textWhite"></textarea>
+            <h4 class="mt-2 mb-4 text-base font-medium ml-1">Details:</h4>
+            <textarea maxlength="50" required v-model="title" placeholder="Title" class="h-16 text-sm w-full min-h-10 p-2 bg-searchBarGray rounded overflow-hidden break-words resize-x whitespace-normal outline-none text-textWhite"></textarea>
+            <textarea maxlength="250" required v-model="description" placeholder="Description" class="h-24 text-sm w-full min-h-10 p-2 bg-searchBarGray rounded overflow-hidden break-words resize-x whitespace-normal outline-none text-textWhite"></textarea>
         </div>
-        <div class="flex gap-2">
-            <h4 class="mt-2 mb-4 text-xl font-medium ml-1">Video:</h4>
-            <div class="relative">
-                <input ref="vidFile" @change="displayFileName" type="file" name="image" id="image" accept="image/*" class="hidden" required>
-                <label ref="vidLabel" for="image" class="cursor-pointer bg-almostBlack hover:bg-gray text-white py-2 px-4 rounded-full">
+        <div class="flex gap-2 mt-3">
+            <h4 class="mt-4 mb-4 text-base font-medium ml-1">Video:</h4>
+            <div class="relative top-2 py-2">
+                <input ref="vidFile" @change="displayFileName" type="file" name="video" id="video" accept="video/*" class="hidden" required>
+                <label ref="vidLabel" for="video" class="cursor-pointer text-sm bg-almostBlack hover:bg-searchBarGray text-white py-2 px-4 rounded-full">
                     {{ vid_message }}
                 </label>
             </div>
         </div>
-        <div class="flex gap-2">
-            <h4 class="mt-2 mb-4 text-xl font-medium ml-1">Thumbnail:</h4>
-            <div class="relative">
+        <div class="flex gap-2 mt-0">
+            <h4 class="mt-4 mb-4 text-base font-medium ml-1">Thumbnail:</h4>
+            <div class="relative top-2 py-2">
                 <input ref="imageFile" @change="displayThumbnailFileName" type="file" name="image" id="image" accept="image/*" class="hidden" required>
-                <label ref="imageLabel" for="image" class="cursor-pointer bg-almostBlack hover:bg-gray text-white py-2 px-4 rounded-full">
+                <label ref="imageLabel" for="image" class="cursor-pointer text-sm bg-almostBlack hover:bg-searchBarGray text-white py-2 px-4 rounded-full">
                     {{ thumbnail_message }}
                 </label>
             </div>
         </div>
-        <div class="flex gap-3">
-            <h4 class="mt-2 mb-4 text-xl font-medium ml-1">Visibility:</h4>
-            <div class="mt-3 text-[1rem] font-light">
-                <input type="radio" :value="true" name="visibility" id="public" v-model="visibility">
-                <label for="public">Public</label>
-            </div>
-            <div class="mt-3 text-[1rem] font-light">
-                <input type="radio" :value="false" name="visibility" id="private" v-model="visibility">
-                <label for="private">Private</label>
+        <div class="flex gap-6 mt-3">
+            <h4 class="mt-[10px] mb-4 text-base font-medium ml-1">Visibility:</h4>
+            <div class="flex gap-2">
+                <div class="mt-3 text-[1rem] font-light">
+                    <input type="radio" :value="true" name="visibility" id="public" v-model="visibility">
+                    <label for="public">Public</label>
+                </div>
+                <div class="mt-3 text-[1rem] font-light">
+                    <input type="radio" :value="false" name="visibility" id="private" v-model="visibility">
+                    <label for="private">Private</label>
+                </div>
             </div>
         </div>
-        <div class="flex justify-end gap-3 text-sm">
-            <button class="py-2 px-3 bg-searchBarGray hover:bg-gray ease-in-out duration-300 rounded-2xl">Cancel</button>
-            <button class="py-2 px-3 bg-highlightBlue hover:bg-[#478ac4] ease-in-out duration-300 rounded-2xl">Upload</button>
+        <div class="flex justify-end gap-3 text-sm mb-8">
+            <button @click="handleCancel" class="py-2 px-3 bg-searchBarGray hover:bg-gray ease-in-out duration-300 rounded-2xl">Cancel</button>
+            <button @click="handleUpload" class="py-2 px-3 bg-highlightBlue hover:bg-[#478ac4] ease-in-out duration-300 rounded-2xl">Upload</button>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default{
     name: 'Upload',
@@ -98,7 +101,7 @@ export default{
                 window.location.reload()
             })
             .catch(err => {
-                window.alert('Error uploading video')
+                window.alert(err.response.data.message)
             })
         }
     }
